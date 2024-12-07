3f0792134c2a577eb1de4ecffca07dc4597d6d21
DallogFheir <dallog.fheir@gmail.com>
2024-12-07 21:28:18 +0100

    refactor: make ReadIntoSplitLines more generic

--- AoC2024/utils/FileOpener.cs
+++ AoC2024/utils/FileOpener.cs
@@ -2,12 +2,9 @@ namespace Aoc2024.Utils
 {
     public static class FileOpener
     {
-        public static T[][] ReadIntoSplitLines<T>(
-            string filePath,
-            Func<string, T[]>? splitter = null
-        )
+        public static T[] ReadIntoSplitLines<T>(string filePath, Func<string, T>? splitter = null)
         {
-            splitter ??= (line) => [(T)(object)line];
+            splitter ??= (line) => (T)(object)line;
 
             string fileContent = File.ReadAllText(filePath);
             string[] lines = fileContent.Split('\n');
